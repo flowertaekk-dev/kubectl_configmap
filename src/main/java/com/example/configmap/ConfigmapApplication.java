ed3ba55fac1e6125dcbdd1d1e2f7dbbb3d2220e3
flowertaekk-dev <flowertaekk.dev@gmail.com>
2022-06-17 22:39:40 +0900

    add spring-boot pj

---
+++ src/main/java/com/example/configmap/ConfigmapApplication.java
@@ -0,0 +1,13 @@
+package com.example.configmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigmapApplication.class, args);
+    }
+
+}
